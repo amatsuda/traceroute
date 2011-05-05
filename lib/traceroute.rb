502d22a86aac8c46e84a1a034066dc41c8db18bf
Akira Matsuda <ronnie@dio.jp>
2011-05-06 02:49:42 +0900

    tie the task

--- lib/traceroute.rb
+++ lib/traceroute.rb
@@ -1,3 +1,7 @@
 module Traceroute
-  # Your code goes here...
+  class Railtie < ::Rails::Railtie
+    rake_tasks do
+      load File.join(File.dirname(__FILE__), 'tasks/traceroute.rake')
+    end
+  end
 end
