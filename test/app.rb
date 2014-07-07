068d0029dabc13683d3e8b5601616df93e3675c9
Akira Matsuda <ronnie@dio.jp>
2014-07-07 18:20:01 +0900

    We don't actually need the controller code

--- test/app.rb
+++ test/app.rb
@@ -14,16 +14,9 @@ end
 class ApplicationController < ActionController::Base; end
 
 class UsersController < ApplicationController
-  def index
-    @users = User.page(params[:page])
-  end
-
-  def index2
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
+  def index; end
+  def index2; end
+  def show; end
 end
 
 DummyApp::Application.initialize!
