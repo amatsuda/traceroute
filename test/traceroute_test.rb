8fd1309881553c4cb14a08de6f7cd352c6cf1e9e
Akira Matsuda <ronnie@dio.jp>
2014-07-08 11:39:25 +0900

    Assert all controller methods - 'rails/*'

--- test/traceroute_test.rb
+++ test/traceroute_test.rb
@@ -3,10 +3,11 @@ require_relative 'test_helper'
 class TracerouteTest < Minitest::Test
   def setup
     @traceroute = Traceroute.new Rails.application
+    @traceroute.load_everything!
   end
 
   def test_defined_action_methods
-    assert_equal ['users#index', 'users#index2', 'users#show', 'admin/shops#index', 'admin/shops#create'], @traceroute.defined_action_methods
+    assert_equal ['users#index', 'users#index2', 'users#show', 'admin/shops#index', 'admin/shops#create'], @traceroute.defined_action_methods.reject {|r| r.start_with? 'rails/'}
   end
 
   def test_routed_actions
