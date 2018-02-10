4c48dd0f043050a3c773ee9c8a9e1cf09fe1bd57
Akira Matsuda <ronnie@dio.jp>
2018-02-11 04:36:48 +0900

    >>

--- test/traceroute_test.rb
+++ test/traceroute_test.rb
@@ -1,106 +1,106 @@
 require_relative 'test_helper'
 
 module TracerouteTest
-class BasicTest < Minitest::Test
-  def setup
-    @traceroute = Traceroute.new Rails.application
-    @traceroute.load_everything!
-  end
-
-  def test_defined_action_methods
-    assert_equal ['users#index', 'users#show', 'users#index2', 'admin/shops#create', 'admin/shops#index'].sort, @traceroute.defined_action_methods.sort
-  end
-
-  def test_routed_actions
-    assert_empty @traceroute.routed_actions
-  end
-end
-
-class RoutedActionsTest < Minitest::Test
-  def setup
-    DummyApp::Application.routes.draw do
-      resources :users, :only => [:index, :show, :new, :create]
-
-      namespace :admin do
-        resources :shops, :only => :index
-      end
+  class BasicTest < Minitest::Test
+    def setup
+      @traceroute = Traceroute.new Rails.application
+      @traceroute.load_everything!
     end
 
-    @traceroute = Traceroute.new Rails.application
-  end
+    def test_defined_action_methods
+      assert_equal ['users#index', 'users#show', 'users#index2', 'admin/shops#create', 'admin/shops#index'].sort, @traceroute.defined_action_methods.sort
+    end
 
-  def teardown
-    DummyApp::Application.routes.clear!
-  end
-
-  def test_routed_actions
-    assert_equal ['admin/shops#index', 'users#index', 'users#show', 'users#new', 'users#create'].sort, @traceroute.routed_actions.sort
-  end
-end
-
-class TracerouteRakeTests < Minitest::Test
-  def setup
-    require 'rake'
-    load "./lib/tasks/traceroute.rake"
-    @fail_on_error_was = ENV['FAIL_ON_ERROR']
-  end
-
-  def teardown
-    ENV['FAIL_ON_ERROR'] = @fail_on_error_was
-    Rake::Task.clear
-    DummyApp::Application.routes.clear!
-  end
-
-  def test_dont_fail_when_envvar_is_anything_but_1
-    traceroute = Traceroute.new Rails.application
-    traceroute.load_everything!
-
-    ENV['FAIL_ON_ERROR'] = "DERP"
-    Rake::Task[:traceroute].execute
-  end
-
-  def test_rake_task_fails_when_unreachable_action_method_detected
-    traceroute = Traceroute.new Rails.application
-    traceroute.load_everything!
-
-    begin
-      ENV['FAIL_ON_ERROR']="1"
-      Rake::Task[:traceroute].execute
-    rescue => e
-      assert_includes e.message, "Unused routes or unreachable action methods detected."
+    def test_routed_actions
+      assert_empty @traceroute.routed_actions
     end
   end
 
-  def test_rake_task_fails_when_unused_route_detected
-    DummyApp::Application.routes.draw do
-      resources :users, :only => [:index, :show, :new, :create] do
-        member do
-          get :index2
+  class RoutedActionsTest < Minitest::Test
+    def setup
+      DummyApp::Application.routes.draw do
+        resources :users, :only => [:index, :show, :new, :create]
+
+        namespace :admin do
+          resources :shops, :only => :index
         end
       end
 
-      namespace :admin do
-        resources :shops, :only => [:index, :create]
-      end
+      @traceroute = Traceroute.new Rails.application
+    end
 
-      namespace :rails do
-        resources :mailers, only: ["index"] do
+    def teardown
+      DummyApp::Application.routes.clear!
+    end
+
+    def test_routed_actions
+      assert_equal ['admin/shops#index', 'users#index', 'users#show', 'users#new', 'users#create'].sort, @traceroute.routed_actions.sort
+    end
+  end
+
+  class TracerouteRakeTests < Minitest::Test
+    def setup
+      require 'rake'
+      load "./lib/tasks/traceroute.rake"
+      @fail_on_error_was = ENV['FAIL_ON_ERROR']
+    end
+
+    def teardown
+      ENV['FAIL_ON_ERROR'] = @fail_on_error_was
+      Rake::Task.clear
+      DummyApp::Application.routes.clear!
+    end
+
+    def test_dont_fail_when_envvar_is_anything_but_1
+      traceroute = Traceroute.new Rails.application
+      traceroute.load_everything!
+
+      ENV['FAIL_ON_ERROR'] = "DERP"
+      Rake::Task[:traceroute].execute
+    end
+
+    def test_rake_task_fails_when_unreachable_action_method_detected
+      traceroute = Traceroute.new Rails.application
+      traceroute.load_everything!
+
+      begin
+        ENV['FAIL_ON_ERROR']="1"
+        Rake::Task[:traceroute].execute
+      rescue => e
+        assert_includes e.message, "Unused routes or unreachable action methods detected."
+      end
+    end
+
+    def test_rake_task_fails_when_unused_route_detected
+      DummyApp::Application.routes.draw do
+        resources :users, :only => [:index, :show, :new, :create] do
           member do
-            get :preview
+            get :index2
+          end
+        end
+
+        namespace :admin do
+          resources :shops, :only => [:index, :create]
+        end
+
+        namespace :rails do
+          resources :mailers, only: ["index"] do
+            member do
+              get :preview
+            end
           end
         end
       end
-    end
 
-    traceroute = Traceroute.new Rails.application
-    traceroute.load_everything!
+      traceroute = Traceroute.new Rails.application
+      traceroute.load_everything!
 
-    begin
-      ENV['FAIL_ON_ERROR'] = "1"
-      Rake::Task[:traceroute].execute
-    rescue => e
-      assert_includes e.message, "Unused routes or unreachable action methods detected."
+      begin
+        ENV['FAIL_ON_ERROR'] = "1"
+        Rake::Task[:traceroute].execute
+      rescue => e
+        assert_includes e.message, "Unused routes or unreachable action methods detected."
+      end
     end
   end
-end
 end
